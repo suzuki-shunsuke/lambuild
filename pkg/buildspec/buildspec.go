f560852d3c5fc956312013e71576c8f67b775338
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-04-30 18:37:08 +0900

    feat: add buidspec package

---
+++ pkg/buildspec/buildspec.go
@@ -0,0 +1,43 @@
+package buildspec
+
+type Buildspec struct {
+	Version string `json:"version"`
+	Env     Env    `json:"env"`
+	Phases  Phases `json:"phases"`
+	Batch   Batch  `json:"batch"`
+}
+
+type Batch struct {
+	BuildGraph []GraphElement `json:"build-graph"`
+}
+
+type GraphElement struct {
+	Identifier    string   `json:"identifier"`
+	Buildspec     string   `json:"buildspec"`
+	DependOn      []string `json:"depend-on" yaml:"depend-on"`
+	Env           GraphEnv `json:"env"`
+	DebugSession  bool     `json:"debug-session" yaml:"debug-session"`
+	IgnoreFailure bool     `json:"ignore-failure" yaml:"ignore-failure"`
+}
+
+type GraphEnv struct {
+	ComputeType    string            `json:"compute-type" yaml:"compute-type"`
+	Image          string            `json:"image"`
+	Type           string            `json:"type"`
+	Variables      map[string]string `json:"variables"`
+	PrivilegedMode bool              `json:"privileged-mode" yaml:"privileged-mode"`
+}
+
+type Env struct {
+	GitCredentialHelper string            `json:"git-credential-helper" yaml:"git-credential-helper"`
+	SecretsManager      map[string]string `json:"secrets-manager" yaml:"secrets-manager"`
+	Variables           map[string]string `json:"variables"`
+}
+
+type Phases struct {
+	Build Phase `json:"build"`
+}
+
+type Phase struct {
+	Commands []string `json:"commands"`
+}
