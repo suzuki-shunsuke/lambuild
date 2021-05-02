3754c69babc5e210277e8102f30a7ecc19db1dc2
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-05-03 08:06:16 +0900

    refactor: remove comment out

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -32,35 +32,3 @@ type Event struct {
 
 	ChangedFiles int
 }
-
-// type Body struct {
-// 	Ref         string      `json:"ref"`
-// 	After       string      `json:"after"`
-// 	Repository  Repository  `json:"repository"`
-// 	Pusher      Pusher      `json:"pusher"`
-// 	Sender      Sender      `json:"sender"`
-// 	HeadCommit  HeadCommit  `json:"head_commit"`
-// 	PullRequest PullRequest `json:"pull_request"`
-// }
-//
-// type PullRequest struct {
-// 	Number int `json:"number"`
-// }
-//
-// type Repository struct {
-// 	Name     string `json:"name"`
-// 	FullName string `json:"full_name"`
-// }
-//
-// type Sender struct {
-// 	Login string `json:"login"`
-// }
-//
-// type Pusher struct {
-// 	Name string `json:"name"`
-// }
-//
-// type HeadCommit struct {
-// 	ID      string `json:"id"`
-// 	Message string `json:"message"`
-// }
