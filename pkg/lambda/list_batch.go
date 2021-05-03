2283595bca14cea7fe9bbc3b6ca666ecb83cc178
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-05-03 21:11:22 +0900

    refactor: split function

--- pkg/lambda/list_batch.go
+++ pkg/lambda/list_batch.go
@@ -24,60 +24,10 @@ func (handler *Handler) handleList(ctx context.Context, logE *logrus.Entry, data
 	}
 
 	if len(listElems) == 1 {
-		listElem := listElems[0]
-		input := &codebuild.StartBuildInput{
-			BuildspecOverride: aws.String(listElem.Buildspec),
-			ProjectName:       aws.String(repo.CodeBuild.ProjectName),
-			SourceVersion:     aws.String(data.SHA),
-		}
-
-		if err := handler.setBuildStatusContext(data, input); err != nil {
+		input, err := handler.getListBuildInput(data, repo, listElems[0])
+		if err != nil {
 			return err
 		}
-
-		if listElem.DebugSession {
-			input.DebugSessionEnabled = aws.Bool(true)
-		}
-
-		if len(listElem.Env.Variables) == 0 {
-			listElem.Env.Variables = make(map[string]string, len(data.Lambuild.Env.Variables))
-		}
-		for k, prog := range data.Lambuild.Env.Variables {
-			if _, ok := listElem.Env.Variables[k]; ok {
-				continue
-			}
-			a, err := runExpr(prog, data)
-			if err != nil {
-				return err
-			}
-			s, ok := a.(string)
-			if !ok {
-				return errors.New("the evaluated result must be string: lambuild.env." + k)
-			}
-			listElem.Env.Variables[k] = s
-		}
-		envs := make([]*codebuild.EnvironmentVariable, 0, len(listElem.Env.Variables))
-		for k, v := range listElem.Env.Variables {
-			envs = append(envs, &codebuild.EnvironmentVariable{
-				Name:  aws.String(k),
-				Value: aws.String(v),
-			})
-		}
-
-		input.EnvironmentVariablesOverride = envs
-
-		if listElem.Env.ComputeType != "" {
-			input.ComputeTypeOverride = aws.String(listElem.Env.ComputeType)
-		}
-
-		if listElem.Env.Image != "" {
-			input.ImageOverride = aws.String(listElem.Env.Image)
-		}
-
-		if listElem.Env.PrivilegedMode {
-			input.PrivilegedModeOverride = aws.Bool(true)
-		}
-
 		buildOut, err := handler.CodeBuild.StartBuildWithContext(ctx, input)
 		if err != nil {
 			return fmt.Errorf("start a batch build: %w", err)
@@ -122,6 +72,63 @@ func (handler *Handler) handleList(ctx context.Context, logE *logrus.Entry, data
 		"build_arn": *buildOut.BuildBatch.Arn,
 	}).Info("start a batch build")
 	return nil
+}
+
+func (handler *Handler) getListBuildInput(data *Data, repo config.Repository, listElem bspec.ListElement) (*codebuild.StartBuildInput, error) {
+	input := &codebuild.StartBuildInput{
+		BuildspecOverride: aws.String(listElem.Buildspec),
+		ProjectName:       aws.String(repo.CodeBuild.ProjectName),
+		SourceVersion:     aws.String(data.SHA),
+	}
+
+	if listElem.Env.ComputeType != "" {
+		input.ComputeTypeOverride = aws.String(listElem.Env.ComputeType)
+	}
+
+	if listElem.Env.Image != "" {
+		input.ImageOverride = aws.String(listElem.Env.Image)
+	}
+
+	if listElem.Env.PrivilegedMode {
+		input.PrivilegedModeOverride = aws.Bool(true)
+	}
+
+	if err := handler.setBuildStatusContext(data, input); err != nil {
+		return nil, err
+	}
+
+	if listElem.DebugSession {
+		input.DebugSessionEnabled = aws.Bool(true)
+	}
+
+	if len(listElem.Env.Variables) == 0 {
+		listElem.Env.Variables = make(map[string]string, len(data.Lambuild.Env.Variables))
+	}
+	for k, prog := range data.Lambuild.Env.Variables {
+		if _, ok := listElem.Env.Variables[k]; ok {
+			continue
+		}
+		a, err := runExpr(prog, data)
+		if err != nil {
+			return nil, err
+		}
+		s, ok := a.(string)
+		if !ok {
+			return nil, errors.New("the evaluated result must be string: lambuild.env." + k)
+		}
+		listElem.Env.Variables[k] = s
+	}
+	envs := make([]*codebuild.EnvironmentVariable, 0, len(listElem.Env.Variables))
+	for k, v := range listElem.Env.Variables {
+		envs = append(envs, &codebuild.EnvironmentVariable{
+			Name:  aws.String(k),
+			Value: aws.String(v),
+		})
+	}
+
+	input.EnvironmentVariablesOverride = envs
+
+	return input, nil
 }
 
 func (handler *Handler) extractBuildList(data *Data, allElems []bspec.ListElement) ([]bspec.ListElement, error) {
