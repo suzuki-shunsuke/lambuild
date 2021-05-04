9d900ce0c9273f3f8d470023e8e61666b22b1ab6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-05-04 11:38:46 +0900

    test: add test

--- pkg/lambda/build_status_context_internal_test.go
+++ pkg/lambda/build_status_context_internal_test.go
@@ -1,8 +1,12 @@
 package lambda
 
 import (
+	"reflect"
 	"testing"
 	"text/template"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/codebuild"
 )
 
 func Test_getBuildStatusContext(t *testing.T) {
@@ -43,6 +47,54 @@ func Test_getBuildStatusContext(t *testing.T) {
 			}
 			if s != d.exp {
 				t.Fatalf("got %s, wanted %s", s, d.exp)
+			}
+		})
+	}
+}
+
+func TestHandler_setBuildStatusContext(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		title string
+		tpl   string
+		input codebuild.StartBuildInput
+		exp   codebuild.StartBuildInput
+		data  Data
+	}{
+		{
+			title: "minimum",
+		},
+		{
+			title: "normal",
+			tpl:   "{{.sha}}",
+			exp: codebuild.StartBuildInput{
+				BuildStatusConfigOverride: &codebuild.BuildStatusConfig{
+					Context: aws.String("0000"),
+				},
+			},
+			data: Data{
+				SHA: "0000",
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			t.Parallel()
+			handler := Handler{}
+			if d.tpl != "" {
+				tpl, err := template.New("_").Parse(d.tpl)
+				if err != nil {
+					t.Fatal(err)
+				}
+				handler.BuildStatusContext = tpl
+			}
+
+			if err := handler.setBuildStatusContext(&d.data, &d.input); err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(d.exp, d.input) {
+				t.Fatalf("got %+v, wanted %+v", d.input, d.exp)
 			}
 		})
 	}
