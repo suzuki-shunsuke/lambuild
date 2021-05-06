4a52daba6ad111ed689f54cfb1fa920e4a0a76c6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-05-06 21:55:03 +0900

    test: add test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -37,3 +37,47 @@ config: foo.yaml
 		})
 	}
 }
+
+func TestConfig_UnmarshalYAML(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		title string
+		src   string
+	}{
+		{
+			title: "normal",
+			src: `region: us-east-1
+repositories:
+  - name: suzuki-shunsuke/lambuild
+    hooks:
+      - {}
+log-level: debug
+build-status-context: yoo
+error-notification-template: foo
+ssm-parameter:
+  parameter-name:
+    github-token: github_token
+    webhook-secret: github_webhook_secret
+`,
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			t.Parallel()
+			cfg := config.Config{}
+			if err := yaml.Unmarshal([]byte(d.src), &cfg); err != nil {
+				t.Fatal(err)
+			}
+			if cfg.LogLevel == 0 {
+				t.Fatal("config.LogLevel is empty")
+			}
+			if cfg.BuildStatusContext == nil {
+				t.Fatal("cfg.BuildStatusContext is nil")
+			}
+			if cfg.ErrorNotificationTemplate == nil {
+				t.Fatal("cfg.ErrorNotificationTemplate is nil")
+			}
+		})
+	}
+}
