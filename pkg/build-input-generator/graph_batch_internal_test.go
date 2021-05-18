3e0958d400eb456ad60b59bf6deeb44b729f9af1
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-05-19 07:22:50 +0900

    test: add tests

--- pkg/build-input-generator/graph_batch_internal_test.go
+++ pkg/build-input-generator/graph_batch_internal_test.go
@@ -6,8 +6,12 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/codebuild"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/sirupsen/logrus"
 	bspec "github.com/suzuki-shunsuke/lambuild/pkg/buildspec"
 	"github.com/suzuki-shunsuke/lambuild/pkg/domain"
+	"github.com/suzuki-shunsuke/lambuild/pkg/expr"
 	"github.com/suzuki-shunsuke/lambuild/pkg/template"
 )
 
@@ -71,6 +75,97 @@ func Test_setGraphBuildInput(t *testing.T) {
 			}
 			if !reflect.DeepEqual(d.exp, d.input) {
 				t.Fatalf("got %+v, wanted %+v", d.input, d.exp)
+			}
+		})
+	}
+}
+
+func Test_extractGraphByDependency(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		title       string
+		identifiers map[string]bspec.GraphElement
+		exp         map[string]bspec.GraphElement
+	}{
+		{
+			title: "minimum",
+		},
+		{
+			title: "remove deploy because test isn't found",
+			identifiers: map[string]bspec.GraphElement{
+				"build": {},
+				"deploy": {
+					DependOn: []string{"test"},
+				},
+			},
+			exp: map[string]bspec.GraphElement{
+				"build": {},
+			},
+		},
+	}
+	logE := logrus.WithFields(logrus.Fields{})
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			t.Parallel()
+			extractGraphByDependency(d.identifiers, logE)
+			if diff := cmp.Diff(d.exp, d.identifiers, cmpopts.IgnoreUnexported(expr.Bool{})); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
+
+func Test_extractGraphByIf(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		title    string
+		data     *domain.Data
+		allElems []bspec.GraphElement
+		exp      map[string]bspec.GraphElement
+	}{
+		{
+			title: "minimum",
+			exp:   map[string]bspec.GraphElement{},
+		},
+		{
+			title: "normal",
+			data:  &domain.Data{},
+			allElems: []bspec.GraphElement{
+				{
+					Identifier: "always",
+				},
+				{
+					Identifier: "false",
+					If:         newBool("false"),
+				},
+				{
+					Identifier: "true",
+					If:         newBool("true"),
+				},
+			},
+			exp: map[string]bspec.GraphElement{
+				"always": {
+					Identifier: "always",
+				},
+				"true": {
+					Identifier: "true",
+					If:         newBool("true"),
+				},
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			t.Parallel()
+			identifiers := map[string]bspec.GraphElement{}
+			err := extractGraphByIf(d.data, d.allElems, identifiers)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(d.exp, identifiers, cmpopts.IgnoreUnexported(expr.Bool{})); diff != "" {
+				t.Fatal(diff)
 			}
 		})
 	}
