a0fea3cf166329f9fa834bea4cdc91912bc000f3
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-05-20 20:12:15 +0900

    test: add tests

--- pkg/buildspec/command.go
+++ pkg/buildspec/command.go
@@ -14,53 +14,45 @@ type Command struct {
 
 type Commands []Command
 
-func (commands *Commands) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var val []interface{}
+func (command *Command) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var val interface{}
 	if err := unmarshal(&val); err != nil {
 		return err
 	}
-	arr := make(Commands, len(val))
-	for i, a := range val {
-		if s, ok := a.(string); ok {
-			arr[i] = Command{
-				Command: s,
-			}
-			continue
-		}
-		b, ok := a.(map[interface{}]interface{})
-		if !ok {
-			return errors.New("command must be string or map[interface{}]interface{}")
-		}
-		cmd := Command{}
-		for k, v := range b {
-			ks, ok := k.(string)
-			if !ok {
-				return errors.New("key of command must be string")
-			}
-			switch ks {
-			case "command":
-				c, ok := v.(string)
-				if !ok {
-					return errors.New("command of command must be string")
-				}
-				cmd.Command = c
-			case "if":
-				vs, ok := v.(string)
-				if !ok {
-					return errors.New("if of buildspec must be string")
-				}
-				prog, err := expr.NewBool(vs)
-				if err != nil {
-					return fmt.Errorf("compile an expression: %s: %w", vs, err)
-				}
-				cmd.If = prog
-			default:
-				return errors.New("invalid key of command: " + ks)
-			}
-		}
-		arr[i] = cmd
+	if s, ok := val.(string); ok {
+		command.Command = s
+		return nil
 	}
-	*commands = arr
+	b, ok := val.(map[interface{}]interface{})
+	if !ok {
+		return errors.New("command must be string or map[interface{}]interface{}")
+	}
+	for k, v := range b {
+		ks, ok := k.(string)
+		if !ok {
+			return errors.New("key of command must be string")
+		}
+		switch ks {
+		case "command":
+			c, ok := v.(string)
+			if !ok {
+				return errors.New("command of command must be string")
+			}
+			command.Command = c
+		case "if":
+			vs, ok := v.(string)
+			if !ok {
+				return errors.New("if of buildspec must be string")
+			}
+			prog, err := expr.NewBool(vs)
+			if err != nil {
+				return fmt.Errorf("compile an expression: %s: %w", vs, err)
+			}
+			command.If = prog
+		default:
+			return errors.New("invalid key of command: " + ks)
+		}
+	}
 	return nil
 }
 
--- pkg/buildspec/command_test.go
+++ pkg/buildspec/command_test.go
@@ -4,9 +4,50 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	bspec "github.com/suzuki-shunsuke/lambuild/pkg/buildspec"
 	"github.com/suzuki-shunsuke/lambuild/pkg/expr"
+	"gopkg.in/yaml.v2"
 )
+
+func TestCommand_UnmarshalYAML(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		title string
+		yaml  string
+		exp   bspec.Command
+	}{
+		{
+			title: "minimum",
+			yaml:  "foo",
+			exp: bspec.Command{
+				Command: "foo",
+			},
+		},
+		{
+			title: "normal",
+			yaml: `command: foo
+if: "true"`,
+			exp: bspec.Command{
+				Command: "foo",
+				If:      expr.NewBoolForTest(t, "true"),
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			t.Parallel()
+			cmd := bspec.Command{}
+			if err := yaml.Unmarshal([]byte(d.yaml), &cmd); err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(d.exp, cmd, cmpopts.IgnoreUnexported(expr.Bool{})); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
 
 func TestCommands_Filter(t *testing.T) {
 	t.Parallel()
