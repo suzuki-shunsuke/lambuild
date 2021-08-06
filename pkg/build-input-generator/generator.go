33f10807e1817bcf14605cd0cdbbb8516c0d645a
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-08-06 20:06:37 +0900

    refactor: remove unused code

--- pkg/build-input-generator/generator.go
+++ pkg/build-input-generator/generator.go
@@ -3,8 +3,6 @@ package generator
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/codebuild"
 	"github.com/sirupsen/logrus"
 	bspec "github.com/suzuki-shunsuke/lambuild/pkg/buildspec"
 	"github.com/suzuki-shunsuke/lambuild/pkg/config"
@@ -28,47 +26,4 @@ func GenerateInput(logE *logrus.Entry, buildStatusContext template.Template, dat
 	}
 
 	return handleBuild(data, buildspec)
-}
-
-func setEnvsToStartBuildInput(input *codebuild.StartBuildInput, data *domain.Data, lambuild bspec.Lambuild, envVars map[string]string) error {
-	envMap := make(map[string]string, len(envVars)+len(lambuild.Env.Variables))
-	for k, prog := range lambuild.Env.Variables {
-		s, err := prog.Run(data.Convert())
-		if err != nil {
-			return fmt.Errorf("evaluate an expression: %w", err)
-		}
-		envMap[k] = s
-	}
-	for k, v := range envVars {
-		envMap[k] = v
-	}
-
-	if len(envMap) == 0 {
-		return nil
-	}
-
-	envs := make([]*codebuild.EnvironmentVariable, 0, len(envMap))
-	for k, v := range envMap {
-		envs = append(envs, &codebuild.EnvironmentVariable{
-			Name:  aws.String(k),
-			Value: aws.String(v),
-		})
-	}
-	input.EnvironmentVariablesOverride = envs
-	return nil
-}
-
-func getLambuildEnvVars(data *domain.Data, lambuild bspec.Lambuild) ([]*codebuild.EnvironmentVariable, error) {
-	envs := make([]*codebuild.EnvironmentVariable, 0, len(lambuild.Env.Variables))
-	for k, prog := range lambuild.Env.Variables {
-		s, err := prog.Run(data.Convert())
-		if err != nil {
-			return nil, fmt.Errorf("evaluate an expression: %w", err)
-		}
-		envs = append(envs, &codebuild.EnvironmentVariable{
-			Name:  aws.String(k),
-			Value: aws.String(s),
-		})
-	}
-	return envs, nil
 }
